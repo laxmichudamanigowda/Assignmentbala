f383b5b569b012563fe6a003b2673a1d80b8867f
laxmichudamanigowda <122073914+laxmichudamanigowda@users.noreply.github.com>
2025-03-12 12:08:14 +0530

    Create variables.tf

---
+++ infra/modules/IAM/variables.tf
@@ -0,0 +1,9 @@
+variable "iam_role_name" {
+  description = "Name of the IAM role for Lambda"
+  type        = string
+}
+
+variable "iam_policy_name" {
+  description = "Name of the IAM policy for Lambda"
+  type        = string
+}
